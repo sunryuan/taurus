bc5f5551f8e17d981f0516a27cc930e0760df21c
Hao Zhu <ainilife@gmail.com>
2012-12-06 14:22:10 +0800

    extract schedule.js from schedule.jsp

---
+++ taurus-web/src/main/webapp/js/schedule.js
@@ -0,0 +1,52 @@
+var taskID;
+var action_type;
+var action_chinese;
+function action(id, action) {
+	taskID = id;
+	action_type = action;
+	if (action == 'delete') {
+		action_chinese = '删除';
+		$("#id_header").html("删除");
+		$("#id_body").html("确定要删除任务<strong>" + id + "</strong>");
+
+	} else if (action == 'suspend') {
+		action_chinese = '暂停';
+		$("#id_header").html("暂停");
+		$("#id_body").html("确定要暂停任务<strong>" + id + "</strong>");
+	} else if (action == 'execute') {
+		action_chinese = '立刻执行';
+		$("#id_header").html("立刻执行");
+		$("#id_body").html("确定要立刻执行任务<strong>" + id + "</strong>");
+	} else if (action == 'resume') {
+		action_chinese = '恢复';
+		$("#id_header").html("恢复");
+		$("#id_body").html("确定要恢复任务<strong>" + id + "</strong>");
+	}
+	$("#confirm").modal('toggle');
+}
+
+function action_ok() {
+	$.ajax({
+		url : "./tasks",
+		data : {
+			action : action_type,
+			id : taskID
+		},
+		type : 'POST',
+		statusCode : {
+			200 : function() {
+				$("#alertContainer").html('<div id="alertContainer" class="alert alert-success"><button type="button" class="close" data-dismiss="alert">&times;</button> <strong>'
+						+ action_chinese + '成功</strong></div>');
+				$(".alert").alert();
+				$('#confirm').modal("hide");
+				$('#' + taskID).remove();
+			},
+			400 : function() {
+				$("#alertContainer").html('<div id="alertContainer" class="alert alert-error"><button type="button" class="close" data-dismiss="alert">&times;</button> <strong>'
+						+ action_chinese + '失败</strong></div>');
+				$(".alert").alert();
+				$('#confirm').modal("hide");
+			}
+		}
+	})
+}
