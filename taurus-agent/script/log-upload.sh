7f1f8300fe7a6bcc45a5b5598d443154aa1aa0b1
renyuan.sun <renyuan.sun@dianping.com>
2012-12-26 10:55:13 +0800

    modify task log html

--- taurus-agent/script/log-upload.sh
+++ taurus-agent/script/log-upload.sh
@@ -6,7 +6,10 @@ if [ $? != 0 ] ; then
         exit 1
 fi
 kinit -R;
+echo "<html><body><h1>sysout</h1>" >>$3
 tail -c 1000000 $1 >>$3
+echo "<h1>syserror</h1>" >>$3
 tail -c 100000 $2 >>$3
+echo "</body></html>" >>$3
 hadoop fs -moveFromLocal $3 $log$4
 exit $?
