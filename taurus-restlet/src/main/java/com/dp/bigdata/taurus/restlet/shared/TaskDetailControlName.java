6a2a815cd452efba66c8fe6b71570de9fe4216a6
Hao Zhu <ainilife@gmail.com>
2013-01-29 14:48:38 +0800

    Bug Fix

---
+++ taurus-restlet/src/main/java/com/dp/bigdata/taurus/restlet/shared/TaskDetailControlName.java
@@ -0,0 +1,43 @@
+package com.dp.bigdata.taurus.restlet.shared;
+
+/**
+ * 
+ * GWTTaskDetailControlName
+ * @author damon.zhu
+ *
+ */
+public enum TaskDetailControlName {
+
+    TASKNAME("taskName"),
+    TASKTYPE("taskType"),
+    TASKPOOL("poolId"),
+    TASKHOSTNAME("hostname"),
+    TAKSSTATE("taskState"),
+    TASKFILE("taskFile"),
+    TASKCOMMAND("taskCommand"),
+    MULTIINSTANCE("multiInstance"),
+    CRONTAB("crontab"),
+    DEPENDENCY("dependency"),
+    PROXYUSER("proxyUser"),
+    MAXEXECUTIONTIME("maxExecutionTime"),
+    MAXWAITTIME("maxWaitTime"),
+    ISAUTORETRY("isAutoRetry"),
+    RETRYTIMES("retryTimes"),
+    TASKID("taskId"),//TODO
+    CREATOR("creator"),
+    DESCRIPTION("description"),
+    ALERTCONDITION("alertCondition"),
+    ALERTTYPE("alertType"),
+    ALERTGROUP("alertGroup"),
+    ALERTUSER("alertUser");
+    
+    private String name;
+    
+    TaskDetailControlName(String name){
+        this.name = name;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+}
