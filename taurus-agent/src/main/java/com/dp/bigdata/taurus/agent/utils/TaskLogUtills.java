fa6320e161cd52406fb77ad6bbf0a2f4512b1091
renyuan.sun <renyuan.sun@dianping.com>
2013-02-21 11:07:10 +0800

    shell to java

---
+++ taurus-agent/src/main/java/com/dp/bigdata/taurus/agent/utils/TaskLogUtills.java
@@ -0,0 +1,28 @@
+/**
+ * Project: taurus-agent
+ * 
+ * File Created at 2013-2-6
+ * $Id$
+ * 
+ * Copyright 2013 dianping.com.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Dianping Company. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with dianping.com.
+ */
+package com.dp.bigdata.taurus.agent.utils;
+
+/**
+ * upload task log
+ * @author renyuan.sun
+ *
+ */
+public class TaskLogUtills {
+    public static void uploadLog(){
+        
+    }
+
+}
