8243696b92a6fc3699612c6ef9438ec8c8339739
sunryuan <sunryuan@gmail.com>
2013-08-09 18:32:09 +0800

    modify agent shell scripte

--- taurus-agent/bin/start.sh
+++ taurus-agent/bin/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Start taurus agent
-# Run 'bin/start.sh init' for the first time. Run 'bin/start.sh for the others'
+
 cd `dirname $0`
 cd ..
 
@@ -12,19 +12,21 @@ do
 	fi
 done
 
-if [ "$1" == "init" ]; then
-	find ./script -type f | xargs dos2unix --dos2unix --safe
-	chmod 744 script/*
-	mkdir -p $taurusJobPath/running
-	chmod 777 $taurusJobPath/running
-	mkdir -p $taurusJobPath/hadoop
-	chmod 777 $taurusJobPath/hadoop
-else
-	id=`ps -ef | grep com.dp.bigdata.taurus.agent.StartServer | grep -v grep | awk '{print $2}'`
-	if [ "$id" != "" ]; then
-		echo "An agent has already started. It will be killed."
-		kill $id
-	fi
-	/usr/local/jdk/bin/java -classpath "conf/:lib/*"  com.dp.bigdata.taurus.agent.StartServer
+#create and modify directory
+find ./script -type f | xargs dos2unix --dos2unix --safe
+chmod 744 script/*
+mkdir -p $taurusJobPath/running
+chmod 777 $taurusJobPath/running
+mkdir -p $taurusJobPath/hadoop
+chmod 777 $taurusJobPath/hadoop
+
+#kill old agent
+id=`ps -ef | grep com.dp.bigdata.taurus.agent.StartServer | grep -v grep | awk '{print $2}'`
+if [ "$id" != "" ]; then
+	echo "An agent has already started. It will be killed."
+	kill $id
 fi
+
+#start agent
+/usr/local/jdk/bin/java -classpath "conf/:lib/*"  com.dp.bigdata.taurus.agent.StartServer
 #end
--- taurus-agent/script/log-upload.sh
+++ taurus-agent/script/log-upload.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 # Upload taurus attempt executing logs
+# Discarded
+
 cd `dirname $0`
+
 source /etc/profile;
 source ~/.bash_profile;
-source ./agent-env.sh
+source ./agent-env.sh ./agent-env.sh
+
 kinit -r 12l -k -t ../conf/taurus.keytab taurus@DIANPING.COM;
 if [ $? != 0 ] ; then
         exit 1
 fi
 kinit -R;
+
 echo '<html><head>    <meta http-equiv="content-type" content="text/html; charset=UTF-8">    <title>log</title>  
 	<style type="text/css"></style><style>.stderr {background-color: #f5ebeb;}.stdout {background-color: #f5ebeb;}
 	body, table td, select {font-family: Arial Unicode MS, Arial, sans-serif;font-size: medium;}</style></head>
@@ -19,4 +24,5 @@ tail -c 100000 $1 |  sed -e "s/$/&<br\/>/g" >>$3
 echo '</div></div></body></html>' >>$3
 hadoop fs -moveFromLocal $3 $taurusLog$4
 exit $?
+
 # end
